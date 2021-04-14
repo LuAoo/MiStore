3bc0b17d6e595681f03c65cd56ba0dabfc6d399c
LuAoo <1399663177@qq.com>
2021-04-14 16:23:43 +0800

    这是在vue中通过设置proxy的方式来处理跨域问题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from "axios"
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+axios({
+  url:'/api/data'
+}).then((result) => {
+  console.log(result);
+})
+ 
+
+
